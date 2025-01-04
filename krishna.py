539f1530c43b8e90a63c6af19fb5f40b08453599
Your Name"k.prasastha <you@example.com"prasasthaists22@gmail.com>
2025-01-04 15:39:06 +0530

    first commit

---
+++ krishna.py
@@ -0,0 +1,3 @@
+krishna=[]
+print(krishna)
+print(type(krishna))
---
+++ mylist.py
@@ -0,0 +1,3 @@
+mylist=[2,2.45,3+6j,(1,2,3),(4,5,6,),{7,8,9,},{"name":"ists"}]
+print(mylist)                                          
+print(type(mylist))
